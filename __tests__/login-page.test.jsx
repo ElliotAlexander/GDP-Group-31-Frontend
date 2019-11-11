8f0f8739c88765cfdc989c6846510a40f5263fb9
Elliot Alexander <ep1e16@soton.ac.uk>
2019-11-11 15:33:07 +0000

    Fixed unused import

---
+++ __tests__/login-page.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import LoginPage from '../src/components/login-page/index.jsx';
+
+describe('Login Page', () => {
+  const wrap = mount(<LoginPage />);
+
+  it('renders successfully', () => {
+    expect(wrap.exists()).toBe(true);
+  });
+});
