dc3d7f630862a9a6e945d80f20179f5a301d6e97
Desislava Stamenova <ds1g16soton.ac.uk>
2019-11-26 15:05:54 +0000

    Forgot to delete a comment..
    - Deleted the comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,3 @@
-// import React, { Component } from 'react';
-// import { hot } from 'react-hot-loader/root';
-// import { Provider } from 'react-redux';
-// import LoginPage from './components/login-page';
-// import { store } from './misc/helpers/store';
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {};
-//   }
-
-//   render() {
-//     return (
-//       <Provider store={store}>
-//         <LoginPage />;
-//       </Provider>
-//     );
-//   }
-// }
-
-// export default hot(App);
-
-// ////////////////////////////////
 import React, { Component } from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 import { hot } from 'react-hot-loader/root';
