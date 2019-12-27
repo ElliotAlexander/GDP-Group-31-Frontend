afd56a9baf9ba15d6914eac9f082b09196288918
Elliot Alexander <ep1e16@soton.ac.uk>
2019-12-27 14:33:47 +0000

    Addded query strings

--- src/components/dashboard/device-panel/index.jsx
+++ src/components/dashboard/device-panel/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import queryString from 'query-string';
+import PropTypes from 'prop-types';
 import DeviceGrid from './DeviceGrid';
 
 class DevicePanel extends React.Component {
+  componentDidMount() {
+    const { location } = this.props;
+    const values = queryString.parse(location.search);
+    console.log(values.device); // "top"
+  }
+
   render() {
     return (
       <div>
@@ -11,4 +19,11 @@ class DevicePanel extends React.Component {
     );
   }
 }
+
+DevicePanel.propTypes = {
+  location: PropTypes.shape({
+    search: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
 export default DevicePanel;
