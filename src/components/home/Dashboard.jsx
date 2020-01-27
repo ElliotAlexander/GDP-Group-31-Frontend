09bd524f923d792e7b4aeea79a7d2a3562126158
Desislava Stamenova <ds1g16soton.ac.uk>
2020-01-27 11:38:46 +0000

    Fixed home page padding (I think)

--- src/components/home/Dashboard.jsx
+++ src/components/home/Dashboard.jsx
@@ -1,47 +1,32 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-import { makeStyles } from '@material-ui/core/styles';
-import { indigo } from '@material-ui/core/colors';
+// import { makeStyles } from '@material-ui/core/styles';
 import { withRouter } from 'react-router-dom';
 
 import Sidebar from '../misc/sidebar/Sidebar';
 import DashboardGrid from './DashboardGrid';
 
-const useStyles = makeStyles(theme => ({
-  title: {
-    flexGrow: 1,
-  },
-  appBarSpacer: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-    paddingTop: '75px',
-  },
-  container: {
-    paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(4),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
-    background: indigo,
-  },
-  fixedHeight: {
-    height: 600,
-  },
-}));
+// const useStyles = makeStyles(theme => ({
+//   title: {
+//     flexGrow: 1,
+//   },
+//   appBarSpacer: theme.mixins.toolbar,
+//   content: {
+//     flexGrow: 1,
+//     height: '100vh',
+//     overflow: 'auto',
+//     paddingTop: '75px',
+//   },
+// }));
 
 function Dashboard() {
-  const classes = useStyles();
+  // const classes = useStyles();
 
   return (
     <Sidebar>
       <CssBaseline />
-      <main className={classes.content}>
+      <main>
         <DashboardGrid />
       </main>
     </Sidebar>
