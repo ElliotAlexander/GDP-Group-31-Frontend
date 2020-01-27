78a5eb9b2ae950d29823b615fd30f66dac63dfd0
Elliot Alexander <ep1e16@soton.ac.uk>
2020-01-27 19:57:00 +0000

    Added device graph fixes

--- src/components/home/panels/DataInOutGraph.jsx
+++ src/components/home/panels/DataInOutGraph.jsx
@@ -1,110 +1,159 @@
-// import React from 'react';
-// import { gql } from 'apollo-boost';
-// import { useQuery } from '@apollo/react-hooks';
-// import CircularProgress from '@material-ui/core/CircularProgress';
-// import { makeStyles } from '@material-ui/core/styles';
-// import PropTypes from 'prop-types';
-// import { Line } from 'react-chartjs-2';
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
+import { Line } from 'react-chartjs-2';
 
-// const useStyles = makeStyles({
-//   container: {},
-// });
+const useStyles = makeStyles({
+  container: {},
+});
 
-// const DEVICES_DATA_QUERY = gql`
-//   query DataInOut {
-//     allDeviceStatsOverTimes(last: 10, orderBy: TIMESTAMP_ASC) {
-//       nodes {
-//         dataIn
-//         dataOut
-//         timestamp
-//       }
-//     }
-//   }
-// `;
+const DEVICES_DATA_QUERY = gql`
+  query MyQuery {
+    allDeviceDataSumOverTimes(orderBy: TIMESTAMP_ASC, last: 50) {
+      edges {
+        node {
+          dataOut
+          dataIn
+          dataTransferred
+          timestamp
+        }
+      }
+    }
+  }
+`;
 
-// function convertBytesToHumanReadable(byteCount) {
-//   if (byteCount < 1000) {
-//     return `${byteCount} Bytes`;
-//   }
+function convertBytesToHumanReadable(byteCount) {
+  if (byteCount < 1000) {
+    return `${byteCount} Bytes`;
+  }
 
-//   const KbCount = byteCount / 1000;
-//   if (KbCount > 1000) {
-//     const MbCount = Math.round((KbCount / 1000) * 100) / 100;
-//     return `${MbCount} Mb`;
-//   }
-//   const KbCountRounded = Math.round(KbCount * 100) / 100;
-//   return `${KbCountRounded} Kb`;
-// }
+  const KbCount = byteCount / 1000;
+  if (KbCount > 1000) {
+    const MbCount = Math.round((KbCount / 1000) * 100) / 100;
+    return `${MbCount} Mb`;
+  }
+  const KbCountRounded = Math.round(KbCount * 100) / 100;
+  return `${KbCountRounded} Kb`;
+}
 
-// function DataInOutGraph(props) {
-//   const classes = useStyles();
-//   const { device } = props;
-//   const { uuid } = device;
-//   const { loading, error, data } = useQuery(DEVICES_DATA_QUERY, {
-//     variables: { uuid },
-//     skip: !uuid,
-//   });
+function DataInOutGraph() {
+  const classes = useStyles();
+  const { loading, error, data } = useQuery(DEVICES_DATA_QUERY);
 
-//   if (loading)
-//     return <CircularProgress id="loading" className={classes.load} />;
-//   if (error)
-//     return (
-//       <p id="error" className={classes.load}>
-//         Error :(
-//       </p>
-//     );
-//   console.log(data);
-//   const timestamps = [];
-//   const dataPoints = [];
-//   for (let i = 0; i < data.allDeviceStatsOverTimes.nodes.length; i += 1) {
-//     timestamps.push(
-//       new Date(data.allDeviceStatsOverTimes.nodes[i].timestamp).getTime(),
-//     );
-//     dataPoints.push(
-//       parseFloat(
-//         convertBytesToHumanReadable(
-//           data.allDeviceStatsOverTimes.nodes[i].dataTransferred,
-//         ),
-//       ),
-//     );
-//   }
-//   function sumInDataFromAllDevices(data) {
-//     let dataIn = 0;
-//     for (let i = 0; i < data.allDeviceStatsOverTimes.nodes.length; i += 1) {
-//       dataIn += parseFloat(data.allDeviceStatsOverTimes.nodes[i].length);
-//     }
+  if (loading)
+    return <CircularProgress id="loading" className={classes.load} />;
+  if (error)
+    return (
+      <p id="error" className={classes.load}>
+        Error :(
+      </p>
+    );
+  console.log(data);
+  const timestamps = [];
+  const dataPointsDataTransferred = [];
+  for (let i = 0; i < data.allDeviceDataSumOverTimes.edges.length; i += 1) {
+    timestamps.push(
+      new Date(
+        data.allDeviceDataSumOverTimes.edges[i].node.timestamp,
+      ).getTime(),
+    );
+    dataPointsDataTransferred.push(
+      parseFloat(
+        convertBytesToHumanReadable(
+          data.allDeviceDataSumOverTimes.edges[i].node.dataTransferred,
+        ),
+      ),
+    );
+  }
 
-//     return dataIn;
-//   }
-//   function sumOutDataFromAllDevices(data) {
-//     let dataOut = 0;
-//     for (let i = 0; i < data.allDeviceStatsOverTimes.nodes.length; i += 1) {
-//       dataOut += parseFloat(data.allDeviceStatsOverTimes.nodes[i].dataOut);
-//     }
+  const dataPointsDataIn = [];
+  for (let i = 0; i < data.allDeviceDataSumOverTimes.edges.length; i += 1) {
+    dataPointsDataIn.push(
+      parseFloat(
+        convertBytesToHumanReadable(
+          data.allDeviceDataSumOverTimes.edges[i].node.dataIn,
+        ),
+      ),
+    );
+  }
 
-//     return dataOut;
-//   }
-//   const data5 = {
-//     labels: timestamps,
-//     datasets: [
-//       {
-//         label: 'Data Transferred in Mb',
-//         backgroundColor: 'rgba(255,99,132,0.2)',
-//         borderColor: 'rgba(255,99,132,1)',
-//         borderWidth: 1,
-//         hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-//         hoverBorderColor: 'rgba(255,99,132,1)',
-//         data: dataPoints,
-//       },
-//     ],
-//   };
-//   return <h1>hello</h1>;
-// }
+  const dataPointsDataOut = [];
+  for (let i = 0; i < data.allDeviceDataSumOverTimes.edges.length; i += 1) {
+    dataPointsDataOut.push(
+      parseFloat(
+        convertBytesToHumanReadable(
+          data.allDeviceDataSumOverTimes.edges[i].node.dataOut,
+        ),
+      ),
+    );
+  }
 
-// DataInOutGraph.propTypes = {
-//   device: PropTypes.shape({
-//     uuid: PropTypes.string,
-//   }),
-// };
+  const data5 = {
+    labels: timestamps,
+    datasets: [
+      {
+        label: 'Data Transferred in Mb',
+        backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
+        data: dataPointsDataTransferred,
+      },
+      {
+        label: 'Data Downloaded in Mb',
+        backgroundColor: 'rgba(0, 0, 0,0.25)',
+        borderColor: 'rgba(0, 0, 0,0.25)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(0, 0, 0,0.5)',
+        hoverBorderColor: 'rgba(0, 0, 0,1)',
+        data: dataPointsDataIn,
+      },
+      {
+        label: 'Data Downloaded in Mb',
+        backgroundColor: 'rgba(0, 0, 0,0.25)',
+        borderColor: 'rgba(0, 0, 0,0.25)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(0, 0, 0,0.5)',
+        hoverBorderColor: 'rgba(0, 0, 0,1)',
+        data: dataPointsDataOut,
+      },
+    ],
+  };
 
-// export default DataInOutGraph;
+  return (
+    <Line
+      data={data5}
+      options={{
+        backgroundColor: 'lightblue',
+        maintainAspectRatio: false,
+        legend: {
+          labels: {
+            fontColor: 'white',
+          },
+        },
+        scales: {
+          xAxes: [
+            {
+              ticks: {
+                fontColor: 'white',
+                display: false,
+              },
+            },
+          ],
+          yAxes: [
+            {
+              ticks: {
+                fontColor: 'white',
+              },
+            },
+          ],
+        },
+      }}
+    />
+  );
+}
+
+export default DataInOutGraph;
